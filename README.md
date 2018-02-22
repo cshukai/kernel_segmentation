311e8893ccb9dadc90ad1ac11b57ab3d3569c546
sc724 <kz724@outlook.com>
2018-02-22 03:34:35 +0000

    prepare to give up segmentation by shape

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # extraction of kernel regions #
 ## color thresholding##
+
 ### RGB ###
 1. the rgb pattern of background blue (B>60, R<30 , G<30)
 2. potential issue:  some of the yellow kernel has shadow which has simialr RGB pattern to black kernel
+
+##Histogram## 
+1. to graysacle 
+2. threshold twice since there are kernel darker/lighter than  blue 
--- pipe.m
+++ pipe.m
@@ -1,3 +1,22 @@
-red_binary = I(:,:,1)  <30 ;
-green_binary = I(:,:,2)  <30 ;
-blue_binary= I(:,:,3)  >60 ;
+img=imread('DSC_0601.NEF')
+I = imcrop(img)
+originalImage=rgb2gray(I)
+thresholdValue = 30; % 30 too little , 50 too large
+binaryImage = originalImage > thresholdValue; 
+blobMeasurements=regionprops(binaryImage,originalImage,'all')
+allBlobIntensities = [blobMeasurements.MeanIntensity];
+allBlobAreas = [blobMeasurements.Area];
+% Get a list of the blobs that meet our criteria and we need to keep.
+% These will be logical indices - lists of true or false depending on whether the feature meets the criteria or not.
+% for example [1, 0, 0, 1, 1, 0, 1, .....].  Elements 1, 4, 5, 7, ... are true, others are false.
+allowableIntensityIndexes = (allBlobIntensities > 150) & (allBlobIntensities < 220);
+allowableAreaIndexes = allBlobAreas < 2000; % Take the small objects.
+% Now let's get actual indexes, rather than logical indexes, of the  features that meet the criteria.
+% for example [1, 4, 5, 7, .....] to continue using the example from above.
+allAreas = [blobMeasurements.Area];
+allPerims = [blobMeasurements.Perimeter];
+circularities = allPerims .^ 2 ./ (4*pi*allAreas);
+%keeperIndexes = find(allowableIntensityIndexes & allowableAreaIndexes);
+keeperIndexes = find(circularities < 2 & allBlobAreas > 20);  
+
+keeperBlobsImage = ismember(binaryImage, keeperIndexes);
