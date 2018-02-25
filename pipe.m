3e6bc619f0ad5d9f0a1cb1e6985e878eea80395c
sc724 <kz724@outlook.com>
2018-02-25 16:55:23 +0000

    code for removing small objects

--- pipe.m
+++ pipe.m
@@ -1,20 +1,40 @@
-img=imread('DSC_0604.NEF')
+img=imread('DSC_0613.NEF')
 I = imcrop(img)
 originalImage=rgb2gray(I)
-thresholdValue = 70; % 30 too little , 50 too large
+thresholdValue = 130; % 30 too little , 50 too large
 binaryImage = originalImage > thresholdValue; 
+
+
+%remove small object representing kernels in blue/purple
+BW2 = bwareaopen(binaryImage, 3);
+ subplot(3,1,1)
+ imshow(I)
+ subplot(3,1,2)
+ imshow(binaryImage)
+ subplot(3,1,3)
+ imshow(BW2)
+
 blobMeasurements=regionprops(binaryImage,originalImage,'all')
 
+
+
+% test if the bounding boxes are correctly located
+subplot(2,1,1)
 imshow(originalImage);
 axis image; % Make sure image is not artificially stretched because of screen's aspect ratio.
+
 hold on;
 boundaries = bwboundaries(binaryImage);
 numberOfBoundaries = size(boundaries, 1);
+
 for k = 1 : numberOfBoundaries
 	thisBoundary = boundaries{k};
 	plot(thisBoundary(:,2), thisBoundary(:,1), 'g', 'LineWidth', 2);
+	
 end
 hold off;
+subplot(2,1,2)
+imshow(I)
 
 
 %crop out
