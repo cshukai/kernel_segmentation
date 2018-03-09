da57c6129eac2992f63028fbd3b04deb1de724aa
sc724 <kz724@outlook.com>
2018-03-09 05:15:56 +0000

    boundary found

--- random_python/gradient.py
+++ random_python/gradient.py
@@ -2,7 +2,7 @@ from PIL import Image
 import numpy
 from scipy.stats.stats import pearsonr
 
-im = Image.open("DSC_0604.NEF") 
+im = Image.open("DSC_0795.NEF") 
 
 pix = im.load()
 
@@ -10,6 +10,7 @@ width=im.size[0]
 height=im.size[1]
 
 # find where pure background section on the left of the image
+cor_result=[]
 for i in range(width):
     if i<width-1:
         left=[]
@@ -18,4 +19,13 @@ for i in range(width):
             left.append(sum(pix[i,j]))
             right.append(sum(pix[i+1,j]))
         
-        pearsonr(left,right)
+        cor_result.append(pearsonr(left,right))
+
+left = 11
+top = 1
+right = 138
+bottom = height
+cropped_example = im.crop((left, top, right, bottom))
+
+#for i in range(pearsonr):
+#    if i
