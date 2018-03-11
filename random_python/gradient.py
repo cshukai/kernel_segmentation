baf141af41ec79b94079c83ff73917df4c7d522d
sc724 <kz724@outlook.com>
2018-03-11 02:05:56 +0000

    second correlation done

--- random_python/gradient.py
+++ random_python/gradient.py
@@ -9,8 +9,8 @@ pix = im.load()
 width=im.size[0]
 height=im.size[1]
 
-# find where pure background section on the left of the image
-cor_result=[]
+# find where pure background sections are
+cor_result_vertical=[] # side
 for i in range(width):
     if i<width-1:
         left=[]
@@ -19,7 +19,27 @@ for i in range(width):
             left.append(sum(pix[i,j]))
             right.append(sum(pix[i+1,j]))
         
-        cor_result.append(pearsonr(left,right))
+        cor_result_vertical.append(pearsonr(left,right))
+
+cor_result_horizontal=[] # top and botton
+middle=height/2
+for i in range(height):
+    if i<height-1:
+        top=numpy.array([])
+        bot=numpy.array([])
+        for j in range(width):
+            top=numpy.append(top,sum(pix[j,i]))
+            bot=numpy.append(bot,sum(pix[j,i+1]))
+            if numpy.sum(top)==0:
+                top=numpy.add(top,1)
+            if numpy.sum(bot)==0:
+                bot=numpy.add(bot,1)    
+                
+            
+        cor_result_horizontal.append(pearsonr(top,bot))
+
+
+
 
 left = 11
 top = 1
@@ -27,5 +47,11 @@ right = 138
 bottom = height
 cropped_example = im.crop((left, top, right, bottom))
 
-#for i in range(pearsonr):
-#    if i
+
+cutoff=0.99
+for i in range(pearsonr):
+    if i<len(cor_result)-1:
+        
+# find the median  RGB values  of background area
+
+        
