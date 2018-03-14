ab0404eda404eeff8a5fb142ad54fd910c02c31b
sc724 <kz724@outlook.com>
2018-03-14 22:53:36 +0000

    bascially done , but overdone

--- gradient/compareGradient.r
+++ gradient/compareGradient.r
@@ -1,6 +1,5 @@
 library("tiff")
-library("imager")
-library(pracma)
+library("png")
 
 background=readTIFF("DSC_0795.NEFleft.tiff")
 kernel= readTIFF("DSC_0795.NEF.tiff")
@@ -15,7 +14,7 @@ for(i in 1:ncol(background)){
 }
 
 b_1st_total=abs(b_r_1st)+abs(b_g_1st)+abs(b_b_1st)
-x_cutoff=max(b_1st_total)
+y_cutoff=max(b_1st_total)
 
 k_r_1st=NULL
 k_g_1st=NULL
@@ -29,9 +28,19 @@ for(i in 1:ncol(kernel)){
 
 k_1st_total=abs(k_r_1st)+abs(k_g_1st)+abs(k_b_1st)
 
+blackout_idx=NULL
+for(i in 1:ncol(k_1st_total)){
+    this_row_idx=which(k_1st_total[,i]>y_cutoff)
+    for(j in 1:length(this_row_idx)){
+        this_idx=c(this_row_idx[j],i)
+        blackout_idx=rbind(blackout_idx,this_idx)
+    }
+}
 
-kernel.r=as.cimg(kernel)
-gr=imgradient(as.cimg(kernel),"xy")
-png('gr.png')
-plot(gr,layout="row")
-dev.off()
+test_img=kernel
+for(i in 1:nrow(blackout_idx)){
+    test_img[blackout_idx[i,1],blackout_idx[i,2],1]=0
+    test_img[blackout_idx[i,1],blackout_idx[i,2],2]=0
+    test_img[blackout_idx[i,1],blackout_idx[i,2],3]=0
+}
+write(test_img,file='grad_Test.png') 
