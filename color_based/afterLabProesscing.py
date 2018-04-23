ad487ac8287a0b5a20834022b80a664778fc16b7
sc724 <kz724@outlook.com>
2018-04-23 05:43:53 +0000

    done but need to add filter for area selection to get rid of noise

--- color_based/afterLabProesscing.py
+++ color_based/afterLabProesscing.py
@@ -28,7 +28,17 @@ test.save(outname)
 
 #exisiting contour finder
 im3=cv2.imread('shadow_removed_lab.tiff',0)
+im6=cv2.imread('DSC_0785.tiff')
 _, contours, hierc = cv2.findContours(im3,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+im4=im3.copy()
+itr=0
+for cnt in contours:
+    
+    (x,y,w,h) = cv2.boundingRect(cnt)
+    #im5=cv2.rectangle(im4,(x,y),(x+w,y+h),(0,255,0),2)
+    kernel = im6[y:y+h,x:x+w]
+    cv2.imwrite(str(itr)+'.png',kernel)
+    itr=itr+1
 #alignment - gpu
 masked={}
 for i in range(width):
