988c4e5a90951dc5383d9315810835af36f230b6
sc724 <kz724@outlook.com>
2018-05-16 05:36:43 +0000

    ongoing developing for reading image using opencv across nodes

--- preprocessing/paralle.py
+++ preprocessing/paralle.py
@@ -2,21 +2,15 @@ import cv2
 import numpy as np
 import os
 
-# build rdd and take one element for testing purpose
-#L = sc.binaryFiles('hdfs://localhost:9000/*.tiff').take(1)
-images = sc.binaryFiles('hdfs://localhost:9000/*.tiff')
-# assuming this operation actually take two tiffs
-# convert to bytearray and then to np array
 
-def saveImage():
-    
 
-images.foreach(saveImage)
-file_bytes = np.asarray(bytearray(L[0][1]), dtype=np.uint8)
+def rdd2RgbMatrix(img):
+    file_bytes = np.asarray(bytearray(img[0][1]), dtype=np.uint8)
+    R = cv2.imdecode(file_bytes,1)
+    return(R)
 
-# use opencv to decode the np bytes array 
-R = cv2.imdecode(file_bytes,1)
-
-#test 
-#cv2.imwrite("test.tiff",R)
-cv2.imwrite("test2.tiff",R)
+# example: https://stackoverflow.com/questions/28731140/using-pyspark-read-write-2d-images-on-hadoop-file-system
+# this uses take which return a list but first function will return tuple , I guess that is why the runtime error
+# was there
+imageRdds = sc.binaryFiles('hdfs://localhost:9000/*.tiff')
+rgbMs=imageRdds.map(rdd2RgbMatrix)
