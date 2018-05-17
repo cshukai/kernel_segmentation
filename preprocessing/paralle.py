35880cdc8169808633c2d297ecbc0a0758d0a1a2
sc724 <kz724@outlook.com>
2018-05-17 02:14:05 +0000

    azure readimage tested

--- preprocessing/paralle.py
+++ preprocessing/paralle.py
@@ -1,16 +1,24 @@
 import cv2
 import numpy as np
 import os
+import mmlspark
+from mmlspark import toNDArray
 
+IMAGE_PATH="hdfs://localhost:9000/"
+images = spark.readImages(IMAGE_PATH, recursive = True, sampleRatio = 1.0).cache()
+images.printSchema()
+images.select('image.width').show()
 
-
-def rdd2RgbMatrix(img):
-    file_bytes = np.asarray(bytearray(img[0][1]), dtype=np.uint8)
-    R = cv2.imdecode(file_bytes,1)
-    return(R)
-
-# example: https://stackoverflow.com/questions/28731140/using-pyspark-read-write-2d-images-on-hadoop-file-system
-# this uses take which return a list but first function will return tuple , I guess that is why the runtime error
-# was there
-imageRdds = sc.binaryFiles('hdfs://localhost:9000/*.tiff')
-rgbMs=imageRdds.map(rdd2RgbMatrix)
+'''
+                                                                           
+|width|
++-----+
+| 3872|
+| 3872|
+| 3872|
+| 3900|
+| 3900|
+| 3900|
+|  206|
++-----+
+'''
