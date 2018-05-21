462f0004e5fddde09f9fddf15baa4a65984c0cbe
sc724 <kz724@outlook.com>
2018-05-21 05:41:50 +0000

    modify for spark submit

--- preprocessing/paralle.hdfs.py
+++ preprocessing/paralle.hdfs.py
@@ -1,19 +1,29 @@
+#pyspark --num-executors 2 --driver-memory 5g --executor-memory 5g --packages Azure:mmlspark:0.12
 import cv2
 import numpy as np
 import os
+import pyspark
+
+spark = pyspark.sql.SparkSession.builder.appName("MyApp") \
+            .config("spark.jars.packages", "Azure:mmlspark:0.12") \
+            .getOrCreate()
+
 import mmlspark
 from mmlspark import toNDArray
 from mmlspark import ImageTransformer
+from mmlspark import ImageWriter
+from PIL import Image
 
 
 IMAGE_PATH="hdfs://localhost:9000/"
-images = spark.readImages(IMAGE_PATH, recursive = True, sampleRatio = 1.0).cache()
+images = spark.readImages(IMAGE_PATH, recursive = True, sampleRatio = 1.0)
 #images.printSchema()
 #images.select('image.width').show()
 
-tr = (ImageTransformer()                  # images are resized and then cropped
+tr_rgb2lab = (ImageTransformer()                  # images are resized and then cropped
       .setOutputCol("transformed")
-      .resize(height = 200, width = 200)
-      .crop(0, 0, height = 180, width = 180) )
+      .colorFormat(cv2.COLOR_RGB2Lab)
+      )
 
-small = tr.transform(images).select("transformed")
+im_lab = tr_rgb2lab.transform(images).select("transformed")
+ImageWriter.write(im_lab)
