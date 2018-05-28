af1a52d5ae147a25fa3468978058ae43b9d7f273
sc724 <kz724@outlook.com>
2018-05-28 18:27:55 +0000

    benchmarking correaltion based filter

---
+++ preprocessing/crop.parrllel.py
@@ -0,0 +1,33 @@
+#continue from preprocessing/input.parallel.py
+from scipy.stats.stats import pearsonr
+from mmlspark import toNDArray
+from sklearn.cluster import KMeans
+import sklearn.metrics as metrics
+import cProfile
+
+def getCorrMatrix(deeperRow,rawImgArr,is4Column):
+    corMax=[]
+    if(is4Column):
+        for i in range(deeperRow.width):
+            if i<deeperRow.width:
+                left=[]
+                right=[]
+                for j in range(deeperRow.height):
+                    left.append(sum(rawImgArr[i,j,:]))
+                    right.append(sum(rawImgArr[i+1,j,:]))
+                    corMax.append(pearsonr(left,right))
+    else:
+        print("")    
+    return corMax    
+    
+t=cProfile.run(getCorrMatrix(y,z,True))
+
+def ColumnCorrKmean(row):
+    deeperRow=row.image
+    arr=toNDArray(deeperRow)
+    
+
+
+def findBondary(row):
+    deeperRow=row.image
+    arr=toNDArray(deeperRow)       
