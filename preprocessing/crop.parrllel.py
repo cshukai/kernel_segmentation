1d084d15b5ce3e9bc9c3d54cfbaaf3960180b926
sc724 <kz724@outlook.com>
2018-05-28 19:54:25 +0000

    fixing another correlation bug

--- preprocessing/crop.parrllel.py
+++ preprocessing/crop.parrllel.py
@@ -1,21 +1,17 @@
 #continue from preprocessing/input.parallel.py
-from scipy.stats.stats import pearsonr
-from mmlspark import toNDArray
-from sklearn.cluster import KMeans
-import sklearn.metrics as metrics
-import cProfile
+
 
 def getCorrMatrix(deeperRow,rawImgArr,is4Column):
     corMax=[]
     if(is4Column):
         for i in range(deeperRow.width):
-            if i<deeperRow.width:
+            if i<deeperRow.width-1:
                 left=[]
                 right=[]
                 for j in range(deeperRow.height):
-                    left.append(sum(rawImgArr[i,j,:]))
-                    right.append(sum(rawImgArr[i+1,j,:]))
-                    corMax.append(pearsonr(left,right))
+                    left.append(sum(rawImgArr[j,i,:]))
+                    right.append(sum(rawImgArr[j,i+1,:]))
+                corMax.append(pearsonr(left,right))
     else:
         print("")    
     return corMax    
@@ -28,8 +24,7 @@ def ColumnCorrKmean(row):
     return(out)
 
 
-cProfile.run("x=images.foreach(ColumnCorrKmean)")
 
-def findBondary(row):
+def findVerticalBondary(row):
     deeperRow=row.image
     arr=toNDArray(deeperRow)       
